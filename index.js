7aa6aa8d52d3a573c8e386ab33a70dbf9f7d7b11
Ibrahim Kholil <33357137+ibrahimkholil6210@users.noreply.github.com>
2022-05-18 11:16:21 +0600

    chore: added logger to api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
 const cors = require('cors')
 const postsRoute = require("./routes/posts");
 const commentsRoute = require("./routes/comments");
 
 app.use(cors())
 app.use(express.json({ limit: '10kb' }));
+app.use(morgan('dev'))
 
 app.use("/api/v1/posts", postsRoute);
 app.use("/api/v1/comments", commentsRoute);
