1bf9ea7d87dc46e25d69d41ffa5bd45291feafa8
Ibrahim Kholil <33357137+ibrahimkholil6210@users.noreply.github.com>
2022-05-24 22:14:45 +0600

    refactor: model change

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express();
 const morgan = require('morgan')
 const cors = require('cors')
 const postsRoute = require("./routes/posts");
-const commentsRoute = require("./routes/comments");
 
 app.use(cors())
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan('dev'))
 
 app.use("/api/v1/posts", postsRoute);
-app.use("/api/v1/comments", commentsRoute);
 
 const PORT = process.env.PORT || 8080;
 
--- routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const commentController = require('../controller/commentController');
-
-const router = express.Router();
-
-router.route("/").post(commentController.AddComment);
-
-module.exports = router;
