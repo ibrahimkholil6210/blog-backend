a22e33e936c94aa6acdb7d24888ce93048d3f700
Ibrahim Kholil <33357137+ibrahimkholil6210@users.noreply.github.com>
2022-05-26 22:54:54 +0600

    fix: global error handler

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.json({ limit: '10kb' }));
 app.use(morgan('dev'))
 
 app.use("/api/v1/posts", postsRoute);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 
